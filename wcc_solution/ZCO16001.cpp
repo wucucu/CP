4c3c9d4fe90be7e0eb76e67efae45c600f458fc2
wucucu <irge@sina.cn>
2017-07-12 17:51:05 +0800

    k todo ZCO16001

---
+++ wcc_solution/ZCO16001.cpp
@@ -0,0 +1,22 @@
+// https://www.codechef.com/ZCOPRAC/problems/ZCO16001
+
+#include <iostream>
+
+int main() {
+
+    int N, K;
+    std::cin >> N >> K;
+
+    int a[N];
+    int b[N];
+
+    for (int i=0; i<N; i++) {
+        std::cin >> a[i];
+    }
+    for (int i=0; i<N; i++) {
+        std::cin >> b[i];
+    }
+
+    return 0;
+
+}
