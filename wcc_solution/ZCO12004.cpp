5a3592abfb808cd0a0945921bf5661c711179e55
wucucu <irge@sina.cn>
2017-07-19 10:59:07 +0800

    k

--- wcc_solution/ZCO12004.cpp
+++ wcc_solution/ZCO12004.cpp
@@ -17,12 +17,20 @@ int main () {
     for (int i=0; i<N; i++) std::cin >> a[i];
     a[N] = 0;
 
+    // suppose first knight takes the cake, last knight may not
     int c[1000001];
     c[0] = a[0];
-    c[1] = a[1];
+    c[1] = a[0]+a[1];
     for (int i=2; i<N+1; i++) c[i] = std::min(c[i-2], c[i-1]) + a[i];
+    int min1 = c[N];
 
-    std::cout << c[N] << std::endl;
+    // suppose last knight takse the cake, first knight may not
+    c[0] = a[0];
+    c[1] = a[1];
+    for (int i=2; i<N; i++) c[i] = std::min(c[i-2], c[i-1]) + a[i];
+    int min2 = c[N-1];
+
+    std::cout << std::min(min1, min2) << std::endl;
     
     return 0;
 }
