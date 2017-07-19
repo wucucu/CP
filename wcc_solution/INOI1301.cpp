1d2fb5b64d61724d5141a65d464f93f9a50843db
wucucu <irge@sina.cn>
2017-07-19 15:57:05 +0800

    k

--- wcc_solution/INOI1301.cpp
+++ wcc_solution/INOI1301.cpp
@@ -2,7 +2,8 @@
 
 #include <iostream>
 #include <algorithm>
-#include <climits>
+
+#define SMALLEST_NUM -2147483648
 
 int a[1000000], dp1[1000000], dp2[1000000];
 
@@ -18,7 +19,7 @@ int main() {
     for (int i=0; i<N; i++) std::cin >> a[i];
 
     // phrase one
-    for (int i=0; i<k-1; i++) dp1[i] = INT_MIN;
+    for (int i=0; i<k-1; i++) dp1[i] = SMALLEST_NUM;
     dp1[k-1] = 0;
     dp1[k] = a[k];
     for (int i=k+1; i<N; i++) dp1[i] = std::max(dp1[i-1], dp1[i-2]) + a[i];
